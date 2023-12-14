a8c27e233ae5bf60dff40addb9fd0ef4971b3816
TpainMcain <trevorwp77@gmail.com>
2023-12-14 02:10:57 -0600

    add todo class

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,16 @@
 import { Component } from '@angular/core';
 
+export class Todo {
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date, 
+  ){
+
+  }
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -8,10 +19,14 @@ import { Component } from '@angular/core';
 export class ListTodosComponent {
 
 todos = [
-    { id: 1, description: "fold laundry" },
-    { id: 2, description: "do the dishes" },
-    { id: 3, description: "vacuum the living room" },
-    { id: 4, description: "write email to the team" }
+  new Todo(1, "fold laundry", false, new Date()),
+  new Todo(2, "do the dishes", false, new Date()),
+  new Todo(3, "vacuum the living room", false, new Date()),
+  new Todo(4, "write email to the team", false, new Date()),
+    // { id: 1, description: "fold laundry" },
+    // { id: 2, description: "do the dishes" },
+    // { id: 3, description: "vacuum the living room" },
+    // { id: 4, description: "write email to the team" }
   ];
   
 
